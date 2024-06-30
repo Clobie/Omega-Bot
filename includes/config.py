074900b102ccb807b0dcbf98e3b027a85919a6b0
Clobie <calebtalexander@gmail.com>
2024-06-29 20:59:32 -0400

    config and gitignore

---
+++ includes/config.py
@@ -0,0 +1,23 @@
+import os
+
+# Bot settings
+BOT_PREFIX = '.'
+main_color = 0xD75BF4
+error = 0xE02B2B
+success = 0x42F56C
+warning = 0xF59E42
+info = 0x4299F5
+
+# Directory structure
+CWD = os.getcwd()
+DIR_INCLUDES = os.path.join(CWD, "includes")
+DIR_LOGS = os.path.join(CWD, "logs")
+DIR_COGS = os.path.join(CWD, "cogs")
+DIR_DOWNLOADS = os.path.join(CWD, "downloads")
+DIR_ASSETS = os.path.join(CWD, "assets")
+
+os.makedirs(DIR_LOGS, exist_ok=True)
+os.makedirs(DIR_DOWNLOADS, exist_ok=True)
+
+# Reused assets
+THINKING_GIF = DIR_ASSETS = os.path.join(DIR_ASSETS, "bubble-loading.gif")
